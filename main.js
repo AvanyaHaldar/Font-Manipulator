fcece5eccaa81ddcd6825b529bd53ab8a449e4e0
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2022-01-17 15:48:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 function setup() {
     canvas=createCanvas(500,500);
-    canvas.center();
+    canvas.position(700,120);
+
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    video.position(150,150);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+    background("pink");
+    text("Avanya",100,100);
+}
+
+function gotPoses(result) {
+    if (result.length>0) {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log("Posenet is Initialized");
 }
